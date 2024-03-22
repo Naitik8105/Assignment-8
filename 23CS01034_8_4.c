9ec359398def84cae12f5d03b123f7ba8df8a6f3
Naitik8105 <152843637+Naitik8105@users.noreply.github.com>
2024-03-22 13:13:43 +0530

    23CS01034_8_4.c

--- 23CS01034_8_4.c
+++ 23CS01034_8_4.c
@@ -6,28 +6,30 @@ void func(int *arr, int n)
     {
         sum += *(arr + i);
     }
-    printf("%d is repeating more than once.\n", sum - (n * (n + 1) / 2));
-}
-int main()
-{
-    int n;
-    printf("Enter n : ");
-    scanf("%d", &n);
-    int arr[n + 1];
-    printf("Enter the elements of the array : ");
-    for (int i = 0; i < n + 1; i++)
+    int num = sum - (n * (n + 1) / 2);
+    for (int i = 1; i <= n; i++)
     {
-        scanf("%d", arr + i);
+        int flag = 0;
+        for (int j = 0; j < n + 1; j++)
+        {
+            if (*(arr + j) == i)
+            {
+                flag++;
+            }
+            else if (*(arr + j) > n)
+            {
+                printf("Invalid input\n");
+                return;
+            }
+        }
+        if (flag != 1 && flag != 2)
+        {
+            printf("Invalid input\n");
+            return;
+        }
     }
-    func(arr, n);
-    return 0;
+    printf("The number repeating twice is: %d\n", num);
 }
-
-void func(int *arr, int n)
-{
-    
-}
-
 int main()
 {
     int n;
